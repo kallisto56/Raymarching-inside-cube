d57d996a40bd35e863ab519ce7f311a07a49b054
Chernyavsky Andrey <ikenni@gmail.com>
2022-09-11 06:48:42 +0500

    Final commit

---
+++ README.md
@@ -0,0 +1,33 @@
+![](./media/screenshot-1.png)
+![](./media/screenshot-0.png)
+
+## Raymarching inside cube using a fragment shader
+Fragment shader requires two important variables: `origin` and `direction`, which are given as interpolated values between vertices of the cube for any given pixel coordinate. Vertex shader calculates `origin` by transforming camera position into local space of the cube, like this:
+```glsl
+vec3 origin = inverseModel * vec4(cameraPosition, 1.0)
+```
+Because `origin` is positioned in local space of the cube, it becomes very simple to calculate direction of the ray, like this:
+```glsl
+vec3 direction = origin - vertexPosition;
+```
+
+Fragment shader normalizes interpolated `direction`, then goes into for-loop and advances the point on the ray starting from `origin` until it hits something or burns out of `MAX_STEPS`.
+
+----
+
+What's cool is that this approach makes it possible to render each cube with its own position, rotation and scale, this is because fragment shader operates inside clip-space. Another cool thing is that you can render inside and/or outside quads. This means that camera can get inside of a cube, but if we do not need that, we can draw less quads.
+
+Note, that this technique does not work like a portal, so any sdf-primitive that sticks outside of the volume will look odd from a certain angle. To better understand what I mean, try replacing `evaluateScene` with this code and play with the camera:
+```glsl
+float evaluateScene (vec3 point)
+{
+	return sdSphere(point, vec3(0, 0, 1), 0.5);
+}
+```
+
+### References
+Signed distance functions used in this code are copied from the website of Inigo Quilez: (https://iquilezles.org/articles/distfunctions)
+
+----
+
+[!["Buy Me A Coffee"](https://www.buymeacoffee.com/assets/img/custom_images/orange_img.png)](https://www.buymeacoffee.com/kallisto56)
---
+++ fragment.glsl
@@ -0,0 +1,105 @@
+#version 450
+
+// Try uncommenting 'noperspective' to see what happens.
+/* noperspective  */in vec3 fragVertexColor;
+/* noperspective  */in vec3 fragOrigin;
+/* noperspective  */in vec3 fragDirection;
+
+// This is an output variable that will be used by OpenGL
+out vec4 fragOutputColor;
+
+// In this situation, epsilon is the smallest acceptable number,
+// below which, we conclude that we hit the surface of the SDF volume.
+// Change this value to 0.1 to see what happens.
+#define EPSILON 0.001
+
+// The more complex scene is, the more steps you will need.
+// This is especially prominent when camera is at the center
+// of this particular model.
+#define COUNT_STEPS 32
+
+
+float opSubtraction(float d1, float d2) // Inigo Quilez (https://iquilezles.org/articles/distfunctions)
+{
+	return max(-d1, d2);
+}
+
+
+float opSmoothUnion(float d1, float d2, float k) // Inigo Quilez (https://iquilezles.org/articles/distfunctions)
+{
+	float h = clamp(0.5 + 0.5 * (d2 - d1) / k, 0.0, 1.0);
+	return mix( d2, d1, h) - k * h * (1.0 - h);
+}
+
+
+float sdSphere (vec3 point, vec3 sphere, float radius) // Inigo Quilez (https://iquilezles.org/articles/distfunctions)
+{
+	return length(point - sphere.xyz) - radius;
+}
+
+
+float sdBox(vec3 point, vec3 bounds) // Inigo Quilez (https://iquilezles.org/articles/distfunctions)
+{
+	vec3 q = abs(point) - bounds;
+	return length(max(q, 0.0)) + min(max(q.x, max(q.y, q.z)), 0.0);
+}
+
+
+float evaluateScene (vec3 point)
+{
+	// uncomment this line to see what happens,
+	// when object goes outside of the volume.
+	// return sdSphere(point, vec3(0, 0, 1), 0.5);
+	float box = sdBox(point, vec3(0.38));
+	float sphere = sdSphere(point, vec3(0, 0, 0), 0.5);
+	float lhs = opSubtraction(sphere, box);
+	// return lhs; // uncomment to see what this group of instructions is about
+	
+	float blob0 = sdSphere(point, vec3(0, 0.0, +0.25), 0.1);
+	float blob1 = sdSphere(point, vec3(0, 0.0, -0.25), 0.1);
+	float blob2 = sdSphere(point, vec3(+0.25, 0.0, 0.0), 0.1);
+	float blob3 = sdSphere(point, vec3(-0.25, 0.0, 0.0), 0.1);
+	float blob4 = sdSphere(point, vec3(0.0, -0.25, 0.0), 0.1);
+	float blob5 = sdSphere(point, vec3(0.0, 0.25, 0.0), 0.1);
+	
+	float rhs = opSmoothUnion(blob0, blob1, 0.59);
+	// return rhs; // uncomment to see what this group of instructions is about
+
+	rhs = opSmoothUnion(rhs, blob2, 0.15);
+	rhs = opSmoothUnion(rhs, blob3, 0.15);
+	rhs = opSmoothUnion(rhs, blob4, 0.15);
+	rhs = opSmoothUnion(rhs, blob5, 0.15);
+	// return rhs; // uncomment to see what this group of instructions is about
+
+	return min(lhs, rhs);
+}
+
+
+void main ()
+{
+	// Try commenting 'normalize' operation and then move
+	// camera around object to see what happens.
+	vec3 direction = normalize(fragDirection);
+	vec3 point = fragOrigin;
+
+	// ...
+	for (int n = 0; n < COUNT_STEPS; n++)
+	{
+		float distance = evaluateScene(point);
+
+		// Are we close enough to surface?
+		if (distance < EPSILON)
+		{
+			fragOutputColor = vec4(point.xyz, 1);
+			return;
+		}
+
+		// Advance the point along the ray direction using
+		// value we've got from 'evaluateScene'.
+		point += direction * distance;
+	}
+
+	// If we reached this point, it means we did not hit anything within COUNT_STEPS budget.
+	// If you do not want to see quads, use "discard" operation.
+	fragOutputColor = vec4(fragVertexColor.xyz, 1) * vec4(0.5, 0.5, 0.5, 1);
+}
